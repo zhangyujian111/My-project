e93cedf27c491d0abdd1dbfd19678c598080a56b
zhangyujian111 <142076498+zhangyujian111@users.noreply.github.com>
2024-04-25 17:37:18 +0800

    444

--- Assets/AAA.cs
+++ Assets/AAA.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class AAA : MonoBehaviour
 {
+    public int a = 10;
     // Start is called before the first frame update
     void Start()
     {
         print("111");
         print("yoyo,Ð¡¶«Î÷");
+        change(a);
+    }
+
+    void change(int a)
+    {
+        a = 100;
     }
 }
