4090d5d4e069f5a536d2dc4ed8ffaf5ee64b03d6
zhangyujian111 <142076498+zhangyujian111@users.noreply.github.com>
2024-04-25 17:28:40 +0800

    222

--- Assets/AAA.cs
+++ Assets/AAA.cs
@@ -7,12 +7,6 @@ public class AAA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        print("111");
     }
 }
